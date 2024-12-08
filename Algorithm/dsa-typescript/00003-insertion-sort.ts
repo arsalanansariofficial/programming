fb145a8458bbf2c27729d99445039a186aa2832f
Arsalan Ansari <arsalanansariofficial@outlook.com>
2024-12-08 14:30:59 +0530

    Add Insertion Sort

---
+++ Algorithm/dsa-typescript/00003-insertion-sort.ts
@@ -0,0 +1,15 @@
+// Sort an array
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+
+export default function insertionSort(array: number[]) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+
+    while (j > -1 && array[j - 1] > array[j]) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]];
+    }
+  }
+
+  return array;
+}
