a7c1bb628817cf1279bef7e201c546639ab504bb
Arsalan Ansari <arsalanansariofficial@outlook.com>
2024-12-24 16:22:00 +0530

    Add Difference Of Squares

---
+++ Algorithm/exercism/c/difference-of-squares.c
@@ -0,0 +1,19 @@
+unsigned int square_of_sum(unsigned int number);
+unsigned int sum_of_squares(unsigned int number);
+unsigned int difference_of_squares(unsigned int number);
+
+unsigned int sum_of_squares(unsigned int n)
+{
+  return n * (n + 1) * (2 * n + 1) / 6;
+}
+
+unsigned int square_of_sum(unsigned int n)
+{
+  unsigned int sum = (1 + n) * n / 2;
+  return sum * sum;
+}
+
+unsigned int difference_of_squares(unsigned int number)
+{
+  return square_of_sum(number) - sum_of_squares(number);
+}
