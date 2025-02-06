a354d88798dabe488905dd347b1318e5ed3cea46
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-02-06 14:12:04 +0530

    Add Giga Second

---
+++ Algorithm/exercism/c/giga-second.c
@@ -0,0 +1,10 @@
+#include <time.h>
+
+void gigasecond(time_t input, char *output, size_t size);
+
+void gigasecond(time_t input, char *output, size_t size)
+{
+  time_t giga_second = input + 1000000000;
+  struct tm *time_info = gmtime(&giga_second);
+  strftime(output, size, "%Y-%m-%d %H:%M:%S", time_info);
+}
